e63c45d07ccc22916509460caf8132156c1381ba
eliahu-co <hi@eliahu.co>
2022-10-23 16:17:58 +0300

    milestone 1 done +loader

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,10 @@
 // this.symbol = '/company.html?symbol=' + companyObject.symbol;
 
 let companySearcher = null;
+let cardHeader = null;
+let spinner = null;
+const loader = `<div class="spinner-border spinner-border-sm" role="status">
+</div>`;
 
 class Company {
     constructor(companyObject) {
@@ -50,8 +54,8 @@ class CompanySearcher {
     }
 
     async runSearch(e) {
-        console.log('runSearch');
-
+        console.log(spinner)
+        hideSpinner();
         this.searchQuery=document.getElementById('search-input').value;
         const results = await this.getCompanies();
         console.log('results',results);
@@ -65,11 +69,14 @@ class CompanySearcher {
     }
 
     async getCompanies() {
-
+        showSpinner();
         try {
             const url = 'https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=' + this.searchQuery + '&limit=' + this.limit + '&exchange=' + this.exchange;
             const response = await fetch(url);
             const results = await response.json();
+                if (response) {
+                    hideSpinner();
+                }
             return results;
         } catch (e) {
             return [];
@@ -77,5 +84,17 @@ class CompanySearcher {
     }
 }
 window.onload = () => {
+    spinner = document.getElementById('spinner')
+    hideSpinner();
     companySearcher = new CompanySearcher();
 }
+
+function hideSpinner() {
+    spinner.style.display='none';
+} 
+
+function showSpinner() {
+    spinner.style.display='block';
+} 
+
+
